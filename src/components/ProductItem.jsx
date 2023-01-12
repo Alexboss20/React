a6783ab919a9827aa253a7a4aa806f70c19211c8
Alexboss20 <joseboscannava@gmail.com>
2023-01-12 10:07:42 -0600

    agregando cambios al home

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import AppContext from '../context/AppContext';
+import AppContext from '../context/AppContext.js';
 import addToCartImage from '@icons/bt_add_to_cart.svg';
 import '@styles/ProductItem.scss';
 
